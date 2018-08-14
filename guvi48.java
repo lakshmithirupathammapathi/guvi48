dc2ff6c623de18f365f7ba506d4fdcd4ec4d32d3
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-14 11:38:55 +0530

    Create guvi48.java

---
+++ guvi48.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n=5,result=0;
+		 
+		int a[]=new int[5];
+		
+		a[0]=1;
+		
+		a[1]=2;
+		
+		a[2]=3;
+		
+		a[3]=4;
+		
+		a[4]=5;
+		
+		for(int i=0;i<n;i++)
+		 
+		 	result=result+a[i];
+		 	System.out.println("avg of  ("+a[0]+","+a[1]+","+a[2]+","+a[3]+","+a[4]+")  is="+result/n);
+		 	
+		 }
+	}
+guvi4
